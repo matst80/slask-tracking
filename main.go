777268cf6499d797f5ff3a0e19f02f59dade4117
matst80 <mats.tornberg@gmail.com>
2025-04-21 17:58:50 +0200

    add checkout endpoint

--- main.go
+++ main.go
@@ -58,6 +58,7 @@ func run_application() int {
 	mux.HandleFunc("/track/action", TrackHandler(viewHandler, TrackAction))
 	mux.HandleFunc("/track/suggest", TrackHandler(viewHandler, TrackSuggest))
 	mux.HandleFunc("/track/cart", TrackHandler(viewHandler, TrackCart))
+	mux.HandleFunc("/track/enter-checkout", TrackHandler(viewHandler, TrackCheckout))
 	mux.HandleFunc("GET /tracking/suggest", JsonHandler(func(w http.ResponseWriter, r *http.Request) (interface{}, error) {
 		q := r.URL.Query().Get("q")
 		return viewHandler.GetSuggestions(q), nil
