32b4074fe1cbd51df1d7f9a6ddc4c6c67e830c36
matst80 <mats.tornberg@gmail.com>
2025-05-07 08:41:07 +0200

    temp tracking

--- pkg/view/utils.go
+++ pkg/view/utils.go
@@ -16,7 +16,7 @@ func GetSessionContentFromRequest(r *http.Request) *SessionContent {
 	if ip == "" {
 		ip = r.RemoteAddr
 	}
-	log.Printf("session update ip:%s, user_agent:%s", ip, r.UserAgent())
+	log.Printf("session update ip:%s, user_agent:%s, referrer: %s", ip, r.UserAgent(), r.Referer())
 	return &SessionContent{
 		Language:     r.Header.Get("Accept-Language"),
 		UserAgent:    r.UserAgent(),
