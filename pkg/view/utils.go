587f16cf557029ba2f2d12cba0d75fa9a666e518
matst80 <mats.tornberg@gmail.com>
2025-05-20 12:36:25 +0200

    less logs

--- pkg/view/utils.go
+++ pkg/view/utils.go
@@ -1,7 +1,6 @@
 package view
 
 import (
-	"log"
 	"net/http"
 )
 
@@ -16,7 +15,7 @@ func GetSessionContentFromRequest(r *http.Request) *SessionContent {
 	if ip == "" {
 		ip = r.RemoteAddr
 	}
-	log.Printf("session update ip:%s, user_agent:%s, referrer: %s", ip, r.UserAgent(), r.Referer())
+	// log.Printf("session update ip:%s, user_agent:%s, referrer: %s", ip, r.UserAgent(), r.Referer())
 	return &SessionContent{
 		Language:     r.Header.Get("Accept-Language"),
 		UserAgent:    r.UserAgent(),
